46e2b92301798cbffb52529f02f5c425b44d45be
Yoona8 <elena243v@gmail.com>
2019-07-04 19:15:39 +0300

    Добавила сервис для работы с уроком в root модуль

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LessonComponent } from './components/lesson/lesson.component';
+import { LessonService } from './services/lesson.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LessonComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [LessonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
